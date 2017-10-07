2c2963b8c258e853044664c7a13c40729c237b50
LewisAn <wza913248383@gmail.com>
2017-10-07 11:58:47 +1000

    The latest version of a3.py. Almost complete task 1 functions

--- a3.py
+++ a3.py
@@ -80,45 +80,44 @@ class InfoPanel(tk.Frame):
     def __init__(self, master):
         self._imgContainer = {}
         self.dotsSerialize = []
-        
+
         self._master = master
         info_frame = tk.Frame(master)
-        info_frame.pack()
+        _turns_frame = tk.Frame(info_frame)
 
-        self._turns_frame = tk.Frame(info_frame)
-        self._turns_frame.pack(side=tk.LEFT, ipadx=50)
+        companions_frame = tk.Frame(info_frame)
 
-        companions_frame=tk.Frame(info_frame)
-        companions_frame.pack(side=tk.LEFT, expand=True)
-        
         self.dots_frame = Frame(info_frame)
-        self.dots_frame.pack(side=tk.LEFT, expand=True)
-        
-        self._turns_label = tk.Label(self._turns_frame,
-                                     text="", font=(None, 30))
-        self._turns_label.pack(anchor=tk.W, expand=False)
 
-        #Set center image and score next to image
+        self._turns_label = tk.Label(_turns_frame,
+                                     text="", font=(None, 30))
+
+        # Set center image and score next to image
         self.image_register("useless.gif")
         self._useless_image = tk.Label(companions_frame, text="",
                                        font=(None, 40),
                                        image=self.get_image("useless.gif"),
                                        compound="right")
 
+        # Packing all the frames
+        info_frame.pack()
+        _turns_frame.pack(side=tk.LEFT, ipadx=50)
+        companions_frame.pack(side=tk.LEFT, expand=True)
+        self.dots_frame.pack(side=tk.LEFT, expand=True)
+        self._turns_label.pack(anchor=tk.W, expand=False)
         self._useless_image.pack(side=tk.RIGHT)
 
-        #Packing all the frames
-
     def set_turns(self, turn):
-        
-        self._turns_label.config(text = "{}".format(turn))
+        self._turns_label.config(text="{}".format(turn))
 
-    def get_image(self,imageId):
-        
-        return self._imgContainer.get(imageId,"Sorry Please register image first")
+    def get_image(self, imageId):
+        return self._imgContainer.get(imageId, "Sorry Please register image first")
 
     def set_status(self, image_id, count, obj):
-        self._status_label = tk.Label(self.dots_frame, image=image_id, text=count, compound="top")
+
+        self._status_label = tk.Label(self.dots_frame,
+                                      image=image_id,
+                                      text=count, compound="top")
         self._status_label.pack(side=tk.RIGHT)
 
         self.dotsSerialize.append([obj.get_kind(),
@@ -131,9 +130,9 @@ class InfoPanel(tk.Frame):
 
     def set_docts_step(self, obj):
         for num in range(len(self.dotsSerialize)):
-                if obj.get_status()[num][1] is not self.dotsSerialize[num][2]:
-                    self.dotsSerialize[num][3].config(text=obj.get_status()[num][1])
-                    self.dotsSerialize[num][2] = obj.get_status()[num][1]
+            if obj[num][1] is not self.dotsSerialize[num][2]:
+                self.dotsSerialize[num][3].config(text=obj[num][1])
+                self.dotsSerialize[num][2] = obj[num][1]
 
     # functionality
     def image_register(self, imageId=None, load_all=False):
@@ -145,6 +144,46 @@ class InfoPanel(tk.Frame):
                     self._imgContainer[id] = PhotoImage(file=COMPANIONSMANAGER[path])
             else:
                 self._imgContainer[imageId] = PhotoImage(file=COMPANIONSMANAGER[imageId])
+
+class IntervalBar(tk.Canvas):
+
+    def __init__(self, master):
+        self._master = master
+        self._canvas = tk.Canvas(master, bg="white",
+                                        width=500, height=30)
+        self._canvas.pack(side=tk.TOP)
+
+        self.draw_rectangle()
+
+
+    def draw_rectangle(self):
+        
+        self._canvas.create_rectangle(80, 10, 140, 30)
+        self._canvas.create_rectangle(140, 10, 200, 30)
+        self._canvas.create_rectangle(200, 10, 260, 30)
+        self._canvas.create_rectangle(260, 10, 320, 30)
+        self._canvas.create_rectangle(320, 10, 380, 30)
+        self._canvas.create_rectangle(380, 10, 440, 30)
+        
+    def fill_rectangle_blue(self, a, b, c, d):
+        self._canvas.create_rectangle(a, b, c, d, fill='blue')
+
+    def fill_rectangle_blank(self, a, b, c, d):
+         
+        self._canvas.create_rectangle(a, b, c, d, fill='white')
+        
+class CompanionDot(BasicDot):
+    DOT_NAME = "companion"
+
+    def activate(self, position, game, activated):
+        pass
+
+    def adjacent_activated(self, position, game, activated, activated_neighbours):
+        pass
+
+    def get_view_id(self):
+        """(str) Returns a string to identify the image for this dot"""
+        return "{}/{}".format(self.get_name(), + self.get_kind())
 
 
 class DotsApp(object):
@@ -160,29 +199,32 @@ class DotsApp(object):
         master.title("Dots & Co")
         # self define
         self._info_panel = InfoPanel(master)
+        self._interval_bar = IntervalBar(master)
         self._menu(master)
 
         self._playing = True
         self._image_manager = ImageManager('images/dots/', loader=load_image)
 
-        self._turns = 21
-        # Game
-        counts = [10, 15, 25, 25]
+        self._turns = 20
+        self._coordinate = -1
+        # Games
+        counts = [10, 15, 25, 25,10, 15, 25, 25]
         random.shuffle(counts)
         # randomly pair counts with each kind of dot
-        objectives = zip([BasicDot(1), BasicDot(2), BasicDot(4), BasicDot(3)], counts)
+        objectives = zip([BasicDot(1), BasicDot(2), BasicDot(3), BasicDot(4),CompanionDot(1),CompanionDot(2),CompanionDot(3),CompanionDot(4)], counts)
         self._objectives = ObjectiveManager(objectives)
 
+        # --------------------------------------------------------------------------------------
         self._objectivesView = ObjectivesView(master, image_manager=self._image_manager)
         for data in self._objectives.get_status():
             self._info_panel.set_status(self._objectivesView.load_image(data[0], (20, 20)), data[1], data[0])
-
 
         dead_cells = {(2, 2), (2, 3), (2, 4),
                       (3, 2), (3, 3), (3, 4),
                       (4, 2), (4, 3), (4, 4),
                       (0, 7), (1, 7), (6, 7), (7, 7)}
-        self._game = DotGame({BasicDot: 1}, objectives=self._objectives, kinds=(1, 2, 3, 4), size=(8, 8),
+
+        self._game = DotGame({BasicDot: 1,CompanionDot: 1}, objectives=self._objectives, kinds=(1, 2, 3, 4), size=(8, 8),
                              dead_cells=dead_cells)
 
         # Grid View
@@ -263,7 +305,6 @@ class DotsApp(object):
         self._grid_view.draw_connection(start, end,
                                         self._game.grid[start].get_dot().get_kind())
 
-
     def _undo(self, positions):
         """Removes all the given dot connections from the grid view
 
@@ -329,9 +370,9 @@ class DotsApp(object):
 
     def reset(self):
         """Resets the game"""
-
         self._game.reset()
-
+        self._grid_view.draw(self._game.grid)
+        self._info_panel.set_turns(20)
 
     def check_game_over(self):
         """Checks whether the game is over and shows an appropriate message box if so"""
@@ -348,6 +389,7 @@ class DotsApp(object):
     def _drop_complete(self):
         """Handles the end of a drop animation"""
         pass
+
     def _score(self, score):  # pylint: disable=no-self-use
         """Handles change in score
 
@@ -355,8 +397,9 @@ class DotsApp(object):
             score (int): The new score
         """
         self._info_panel.set_score(self._game.get_score())
-        self._info_panel.set_docts_step(self._objectives)
-        self._info_panel.set_turns(self.config_turns())
+        self._info_panel.set_docts_step(self._objectives.get_status())
+        self._info_panel.set_turns(self._game.get_moves())
+        self.config_progress()
 
     def _menu(self, master):
 
@@ -373,14 +416,28 @@ class DotsApp(object):
             self._master.destroy()
         else:
             showinfo('No', 'Welcome back')
-            
-    def config_turns(self):
-        self._turns -= 1
-        if self._turns == 0 :
-            self._playing = False
-        return self._turns
-        
 
+    def config_progress(self):
+        
+        if self._coordinate == 5:
+            
+            self._interval_bar.fill_rectangle_blank(140, 10, 200, 30)
+            self._interval_bar.fill_rectangle_blank(200, 10, 260, 30)
+            self._interval_bar.fill_rectangle_blank(260, 10, 320, 30)
+            self._interval_bar.fill_rectangle_blank(320, 10, 380, 30)
+            self._interval_bar.fill_rectangle_blank(380, 10, 440, 30)
+            self._coordinate = -1
+            
+        canvas_coordinate = [(80, 10, 140, 30),
+                             (140, 10, 200, 30),
+                             (200, 10, 260, 30),
+                             (260, 10, 320, 30),
+                             (320, 10, 380, 30),
+                             (380, 10, 440, 30)]
+        
+        self._coordinate += 1
+        a,b,c,d = canvas_coordinate[self._coordinate]
+        self._interval_bar.fill_rectangle_blue(a,b,c,d)
 
 def main():
     """Sets-up the GUI for Dots & Co"""
